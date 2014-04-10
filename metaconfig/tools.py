9f0627aeb8f2c6cb7e0199396dc62401c7643395
Evgenus <chernyshov.eugene@gmail.com>
2014-04-10 06:28:27 +0300

    Variants resolving primitive

--- metaconfig/tools.py
+++ metaconfig/tools.py
@@ -2,6 +2,9 @@ from abc import abstractmethod, ABCMeta
 import collections
 
 class Primitive(metaclass=ABCMeta):
+    """
+    Base class for callable Lisp-like objects to be defined inside config
+    """
     @abstractmethod
     def __call__(self): pass
 
@@ -14,10 +17,23 @@ class Constant(Primitive):
 
 class Function(Primitive):
     def __init__(self, func, *args):
-        assert isinstance(func, collections.Callable)
+        assert isinstance(func, collections.Callable), func
         self.func = func
         for value in args:
             assert isinstance(value, Primitive)
         self.args = args
     def __call__(self):
         return self.func(*(arg() for arg in self.args))
+
+class Try(Primitive):
+    def __init__(self, *funcs):
+        for func in funcs:
+            assert isinstance(func, collections.Callable)
+        self.funcs = funcs
+    def __call__(self):
+        for func in self.funcs:
+            try:
+                return func()
+            except Exception:
+                continue
+
