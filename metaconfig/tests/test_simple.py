b51e51dc8b1ce66815980bf2e8424f6fe282af66
Evgenus <chernyshov.eugene@gmail.com>
2016-10-22 09:31:44 +0300

    test type declare with resolve

--- metaconfig/tests/test_simple.py
+++ metaconfig/tests/test_simple.py
@@ -15,3 +15,34 @@ def test_declare_empty():
 
     with StringIO(dedent(source)) as stream:
         config.load(stream)
+
+def test_declare_resolve():
+
+    source = """
+    --- !declare
+    type:
+        type: !resolve builtins.type
+        load: !resolve metaconfig.construct_from_sequence
+    ...
+
+    --- !let
+    integer: !type 
+        - 0
+    string: !type 
+        - ""
+    float: !type 
+        - 1.0
+    "null": !type
+        - ~
+    ...
+    """
+
+    config = Config()
+
+    with StringIO(dedent(source)) as stream:
+        config.load(stream)
+
+    eq_(int, config.get("integer"))
+    eq_(str, config.get("string"))
+    eq_(float, config.get("float"))
+    eq_(type(None), config.get("null"))
