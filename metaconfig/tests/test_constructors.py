d9a65e5ecbedfe1e30b64d31ce74f47d94465734
Evgenus <chernyshov.eugene@gmail.com>
2016-10-26 14:46:05 +0300

    test construct from string

--- metaconfig/tests/test_constructors.py
+++ metaconfig/tests/test_constructors.py
@@ -61,3 +61,37 @@ def test_construct_from_sequence():
 
     assert_tuple_equal(value[0], (1, 2))
     assert_dict_equal(value[1], {})
+
+def test_construct_from_string():
+
+    source = """
+    --- !declare
+    func:
+        type: !resolve metaconfig.tests.utils.identity
+        load: !resolve metaconfig.construct_from_string
+    ...
+
+    --- !let
+    value1: !func "blabla"
+    value2: !func 1
+    value3: !func 3.14
+    ...
+    """
+
+    config = Config()
+
+    with StringIO(dedent(source)) as stream:
+        config.load(stream)
+
+    value1 = config.get("value1")
+    assert_tuple_equal(value1[0], ("blabla",))
+    assert_dict_equal(value1[1], {})
+
+    value2 = config.get("value2")
+    assert_tuple_equal(value2[0], ("1",))
+    assert_dict_equal(value2[1], {})
+
+    value3 = config.get("value3")
+    assert_tuple_equal(value3[0], ("3.14",))
+    assert_dict_equal(value3[1], {})
+
