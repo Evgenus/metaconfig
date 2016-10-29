a067268ac0615ed7944b2a9cd52358ce5ed8ef74
Evgenus <chernyshov.eugene@gmail.com>
2016-10-29 03:14:42 +0300

    construct value test

--- metaconfig/tests/test_constructors.py
+++ metaconfig/tests/test_constructors.py
@@ -175,3 +175,52 @@ def test_construct_from_args_kwargs():
     value2 = config.get("value2")
     assert_tuple_equal(value2[0], ())
     assert_dict_equal(value2[1], {"a": 1, "b": 2})
+
+def test_construct_from_value():
+
+    source = """
+    --- !declare
+    func:
+        type: !resolve metaconfig.tests.utils.identity
+        load: !resolve metaconfig.construct_from_value
+    ...
+
+    --- !let
+    value1: !func
+        a: 1
+        c: 2
+        b: 3
+        d: 4
+        f: 5
+        e: 6
+        h: 7
+        g: 8
+    value2: !func
+        - 1
+        - 2
+        - 3
+    value3: !func "test"
+    value4: !func 12345678
+    value5: !func 3.141592
+    ...
+    """
+
+    config = Config()
+
+    with StringIO(dedent(source)) as stream:
+        config.load(stream)
+
+    value1 = config.get("value1")[0]
+    assert_dict_equal(value1[0], {"a": 1, "c": 2, "b": 3, "d": 4, "f": 5, "e": 6, "h": 7, "g": 8})
+    assert_list_equal(list(value1[0].keys()), ["a", "c", "b", "d", "f", "e", "h", "g"])
+    value2 = config.get("value2")[0]
+    assert_list_equal(value2[0], [1, 2, 3])
+
+    value3 = config.get("value3")[0]
+    assert_tuple_equal(value3, ("test",))
+
+    value4 = config.get("value4")[0]
+    assert_tuple_equal(value4, (12345678, ))
+
+    value5 = config.get("value5")[0]
+    assert_tuple_equal(value5, (3.141592, ))
